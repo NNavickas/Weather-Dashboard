e5c746931e8dc257f7665e0a1f15ab9dac909706
natalie navickas <natalienavickas@hotmail.com>
2023-01-30 20:46:45 +0000

    attempt at filling in current city data to the today section

--- js/script.js
+++ js/script.js
@@ -5,7 +5,6 @@ var buttonEl = $("#search-button");
 var historyEl = $("#history");
 var clearEl = $("#clear-button");
 
-
 // API endpoint for geocoding
 const geocodeApiEndpoint = "https://api.openweathermap.org/geo/1.0/direct?";
 
@@ -18,47 +17,85 @@ const apiKey = "7cecd6cf1a25249cb6676d7e0009bd81";
 // Units (imperial or metric)
 const units = "metric";
 
-$("#search-button").on("click", function(event) {
+$("#search-button").on("click", function (event) {
   event.preventDefault();
   // Get the city value from the input box
   const city = $("#search-input").val();
 
   // Build the API query URL for geocoding
-  const geocodeQueryURL = geocodeApiEndpoint + "q=" + city + "&limit=1&appid=" + apiKey;
+  const geocodeQueryURL =
+    geocodeApiEndpoint + "q=" + city + "&limit=1&appid=" + apiKey;
 
   // Make the AJAX request for geocoding
   $.ajax({
     url: geocodeQueryURL,
     type: "GET",
-    success: function(geocodeData) {
+    success: function (geocodeData) {
       // Get the latitude and longitude from the geocodeData
       const lat = geocodeData[0].lat;
       const lon = geocodeData[0].lon;
 
       console.log(geocodeQueryURL);
-      
-      // Build the API query URL for 5-day forecast
-      const forecastQueryURL = forecastApiEndpoint + "lat=" + lat + "&lon=" + lon + "&units=" + units + "&appid=" + apiKey;
 
+      // Build the API query URL for 5-day forecast
+      const forecastQueryURL =
+        forecastApiEndpoint +
+        "lat=" + lat +
+        "&lon=" + lon +
+        "&units=" + units +
+        "&appid=" +apiKey;
 
       // Make the AJAX request for 5-day forecast
       $.ajax({
         url: forecastQueryURL,
         type: "GET",
-        success: function(forecastData) {
+        success: function (forecastData) {
           console.log(forecastData);
           console.log(forecastQueryURL);
         },
-        error: function(error) {
+        error: function (error) {
           console.error("Error getting 5-day forecast: ", error);
-        }
+        },
       });
     },
-    error: function(error) {
+    error: function (error) {
       console.error("Error getting geocode data: ", error);
-    }
+    },
   });
 });
+
+// function to render the searched city current weather
+function displayCurrentCity() {
+
+$.ajax({
+  url: forecastQueryURL,
+  method: "GET"
+}).then(function(forecastData) {
+  // Get the city name from the API response
+  var city = forecastData.list.city.name;
+  
+  // Get the current date
+  var currentDate = new Date();
+  
+  // Get the temperature from the API response
+  var temperature = forecastData.list[0].main.temp;
+  
+  // Get the wind speed from the API response
+  var windSpeed = forecastData.list[0].wind.speed;
+  
+  // Get the humidity from the API response
+  var humidity = forecastData.list[0].main.humidity;
+  
+  // Update the HTML with the extracted data
+  $("#current").text(city + " - " + currentDate.toLocaleDateString());
+  $("#tempDay2").text("Temp: " + temperature + "°C");
+  $("#windDay2").text("Wind Speed: " + windSpeed + "m/s");
+  $("#humidityDay2").text("Humidity: " + humidity + "%");
+  displayCurrentCity();
+});
+console.log(windSpeed);
+}
+
 
 
 //   const apiKey = "&limit=1&appid=7cecd6cf1a25249cb6676d7e0009bd81";
@@ -86,7 +123,7 @@ $("#search-button").on("click", function(event) {
 //   console.log(finalQueryURL);
 // }
 
-// FUNCTION 
+// FUNCTION
 // // Update the page with the data
 // function getCurrentLocation(position) {
 
@@ -103,8 +140,7 @@ $("#search-button").on("click", function(event) {
 
 // };
 
-// Update page function 
-
+// Update page function
 
 // // clear function
 // function clear() {
@@ -127,20 +163,15 @@ $("#search-button").on("click", function(event) {
 // // clear history
 // $("#clear-button").on("click", clear);
 
-
-
-
 // declare variable to handle form submit
 // include a prevent default
 // var handleFormSubmit = function (event) {
 //   event.preventDefault();
 
+// event listener for the search button click
 
-  // event listener for the search button click
-
-  // API request to use the city entered before 'click' as the search term
+// API request to use the city entered before 'click' as the search term
 // };
-
 
 // fetch(`http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid={API_KEY}`)
 //   .then(response => response.json())
@@ -166,5 +197,5 @@ $("#search-button").on("click", function(event) {
 // }
 
 // function nextFunction() {
-    
+
 // }
